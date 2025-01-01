b21e4602048c81c6d2dfde3728cfc59041acee00
fangyixiong <fangyixiong@sjtu.edu.cn>
2025-01-01 18:17:36 +0800

    add log

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
 import whisper
 from utils.speech2text import speech2text
 from utils.translation import translate_cn_to_en
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
 import transformers
 import torch
 
 # prepare log file
-log_file = "tmp/result.log"
+log_file = "tmp/result.md"
 
-def log_message(message):
+def log_message(message, is_code=False):
     with open(log_file, "a") as log:
-        log.write(message + "\n")
+        if is_code:
+            log.write(f"```\n{message}\n```\n")
+        else:
+            log.write(message + "\n\n")
 
 # prepare the model
-log_message("Loading models...")
+log_message("# Loading models...")
 api_key = "3cda741a-a328-4ebd-a687-05462dd804b4:fx"
 tokenizer = AutoTokenizer.from_pretrained("Nan-Do/LeetCodeWizard_7B_V1.1")
 model = AutoModelForCausalLM.from_pretrained("Nan-Do/LeetCodeWizard_7B_V1.1").to("cuda:1")
 speech_model = whisper.load_model("turbo")
 
-# recognize the speech
-log_message("Recognizing speech...")
-question = speech2text("recordings/reverse_list.m4a", speech_model)
-
-# translate the question
-log_message("Translating the question...")
-chinese_text = question
-english_translation = translate_cn_to_en(chinese_text, api_key)
-log_message("Translated question: " + english_translation)
-
-# generate the code
-log_message("Generating answer...")
-instruction = english_translation
-prompt = f"Below is an instruction that describes a task. Write a **code** and **algorithm** for response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:"
+# prepare the pipeline
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
@@ -41,13 +31,41 @@ pipeline = transformers.pipeline(
     tokenizer=tokenizer,
 )
 
-sequences = pipeline(
-    prompt,
-    do_sample=False,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=500,
-)
-for seq in sequences:
-    log_message(f"Answer:")
-    log_message(f"{seq['generated_text'][len(prompt):]}")
+log_message("## Models loaded. Waiting for user input...")
+
+while True:
+    input("Press Enter to process the next speech input...")
+
+    # recognize the speech
+    log_message("### Recognizing speech...")
+    question = speech2text("recordings/reverse_list.m4a", speech_model)
+
+    # translate the question
+    log_message("### Translating the question...")
+    chinese_text = question
+    english_translation = translate_cn_to_en(chinese_text, api_key)
+    log_message("#### Translated question: " + english_translation)
+
+    # generate the code
+    log_message("### Generating answer...")
+    instruction = english_translation
+    prompt = f"Below is an instruction that describes a task. Write a **code** and **algorithm** for response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:"
+
+    # Streaming code generation
+    streamer = TextIteratorStreamer(tokenizer, skip_special_tokens=True)
+
+    generation = model.generate(
+        tokenizer(prompt, return_tensors="pt").input_ids.to("cuda:0"),
+        streamer=streamer,
+        do_sample=False,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=500,
+    )
+
+    log_message("#### Answer:")
+    with open(log_file, "a") as log:
+        # log.write("```\n")
+        for token in streamer:
+            log.write(token)
+            print(token, end="", flush=True)
+        # log.write("\n```\n")
